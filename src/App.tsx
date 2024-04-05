8e08dd631ffda1ffdc71801e9eb6149d1be20e50
czerwonka <czerwooonka@gmail.com>
2024-04-05 13:41:18 +0200

    Project initialization

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="panel">
+        <p>Displaing chart</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
