e501b3925bc14f4ce77a846af1e25d4135332de3
czerwonka <czerwooonka@gmail.com>
2024-04-07 13:21:21 +0200

    Added fetch hook

---
+++ src/hooks/useFetch.tsx
@@ -0,0 +1,30 @@
+import { useState, useEffect } from "react";
+
+interface FetchProps {
+  url: URL;
+}
+
+export const useAxiosFetch = ({ url }: FetchProps) => {
+  const [data, setData] = useState<any>(null);
+  const [error, setError] = useState<any>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  const fetchData = async (): Promise<void> => {
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      setData(data);
+    } catch (error) {
+      setError(error);
+      setLoading(false);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return [data, error, loading] as const;
+};
